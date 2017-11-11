01087bd70502b4f5fad57a4f456b102b7decb886
bahuzz <dbahuzz@gmail.com>
2017-11-12 00:40:22 +0300

    slider portfolio

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,13 @@
-$(document).ready(function(){
- $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-});
-});
+$(document).ready(function() {
+$('.portfolio__carousel').owlCarousel({
+	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 3],[992,3]],
+  	autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+	pagination: false,
+	navigation:true,
+	slideSpeed:500,
+	stagePadding: 50,
+	autoHeight:true
+	}); 
+});	
