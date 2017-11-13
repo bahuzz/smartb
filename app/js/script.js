b2cf32f0aea6a3495409ccf36846f5195fc3d089
bahuzz <dbahuzz@gmail.com>
2017-11-14 01:43:10 +0300

    responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $('.portfolio__carousel').owlCarousel({
-	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 3],[992,3]],
+	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 2],[992,2],[1180,3]],
   	autoplay:true,
     autoplayTimeout:1000,
     autoplayHoverPause:true,
@@ -12,7 +12,7 @@ $('.portfolio__carousel').owlCarousel({
 	}); 
 
 $('.video__carousel').owlCarousel({
-	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 2],[992,2]],
+	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 1],[992,1],[1180,2]],
   	autoplay:true,
     autoplayTimeout:1000,
     autoplayHoverPause:true,
@@ -24,7 +24,7 @@ $('.video__carousel').owlCarousel({
 	});
 
 $('.logo-carousel').owlCarousel({
-	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 4],[992,4]],
+	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 2],[1024,4]],
   	autoplay:true,
     autoplayTimeout:1000,
     autoplayHoverPause:true,
@@ -36,7 +36,7 @@ $('.logo-carousel').owlCarousel({
 	});
 
 $('.reviews-slider').owlCarousel({
-	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 3],[992,3]],
+	itemsCustom: [ [0, 1], [500, 1],[660,1], [767, 2],[1024,3]],
   	autoplay:true,
     autoplayTimeout:1000,
     autoplayHoverPause:true,
@@ -46,4 +46,49 @@ $('.reviews-slider').owlCarousel({
 	stagePadding: 50,
 	autoWidth: true
 	});
+
+$(".diagram1").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 10,
+                foregroundColor: "#ef2452",
+                backgroundBorderWidth: 5,
+                backgroundColor: "#fff",
+                percent: 60
+           });
+
+$(".diagram2").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 10,
+                foregroundColor: "#ca294f",
+                backgroundBorderWidth: 5,
+                backgroundColor: "#fff",
+                percent: 65
+           });
+
+$(".diagram3").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 10,
+                foregroundColor: "#94304c",
+                backgroundBorderWidth: 5,
+                backgroundColor: "#fff",
+                percent: 70
+           });
+
+$(".diagram4").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 10,
+                foregroundColor: "#643648",
+                backgroundBorderWidth: 5,
+                backgroundColor: "#fff",
+                percent: 75
+           });
+
+$(".diagram5").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 10,
+                foregroundColor: "#493a47",
+                backgroundBorderWidth: 5,
+                backgroundColor: "#fff",
+                percent: 82
+           });
 });	
